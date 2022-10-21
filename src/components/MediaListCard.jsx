7bbe5a2b278ab5dedade4f369d6631dfc80a80b1
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-10-21 15:46:18 +0100

    Update

--- src/components/MediaListCard.jsx
+++ src/components/MediaListCard.jsx
@@ -3,13 +3,24 @@ import "./MediaListCard.css";
 import DataJson from "../data/data.json";
 
 function MediaListCard() {
-  const [jsonData, setJsonData] = useState(DataJson);
+  const [jsonData, setJsonData] = useState(DataJson[0].profileDetails);
+  console.log(jsonData);
 
   useEffect(() => {
     setJsonData(jsonData);
   }, [jsonData]);
 
-  return <div>{}</div>;
+  return (
+    <>
+      {jsonData.map((item, index) => {
+        return (
+          <button key={index} className="link_button" id={item.id}>
+            {item.name}
+          </button>
+        );
+      })}
+    </>
+  );
 }
 
 export default MediaListCard;
