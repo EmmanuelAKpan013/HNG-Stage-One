c4666875987024792cf92a9fbf2537ebe8ccd075
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-10-21 14:49:29 +0100

    Profile Page Update

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -3,9 +3,12 @@ import MediaListCard from "../components/MediaListCard";
 
 function ProfilePage() {
   return (
-    <>
+    <div className="Profile_container">
+      <div className="profile_picture_container">
+        <img src={""} alt="" id="profile__img" />
+      </div>
       <MediaListCard />
-    </>
+    </div>
   );
 }
 
