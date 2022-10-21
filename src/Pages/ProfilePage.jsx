db137d737c0e1150b25c65aa067730267d8fbbe5
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-10-21 15:01:40 +0100

    update

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import "./ProfilePage.css";
 import MediaListCard from "../components/MediaListCard";
+import ProfilePicture from "../images/emma.jpg";
 
 function ProfilePage() {
   return (
     <div className="Profile_container">
       <div className="profile_picture_container">
-        <img src={""} alt="" id="profile__img" />
+        <img src={ProfilePicture} alt="" id="profile__img" />
       </div>
       <MediaListCard />
     </div>
