227fdb187cda4ae1d058653bbb8e6b1f956cdcc9
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-10-21 14:33:17 +0100

    Json Data

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import MediaListCard from "../components/MediaListCard";
 
 function ProfilePage() {
-  return <div>ProfilePage</div>;
+  return (
+    <>
+      <MediaListCard />
+    </>
+  );
 }
 
 export default ProfilePage;
--- src/components/MediaListCard.jsx
+++ src/components/MediaListCard.jsx
@@ -1,1 +1,9 @@
 import "./MediaListCard.css";
+
+import React from "react";
+
+function MediaListCard() {
+  return <div>MediaListCard</div>;
+}
+
+export default MediaListCard;
