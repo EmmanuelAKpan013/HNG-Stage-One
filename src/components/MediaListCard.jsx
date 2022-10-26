d40f11dd4db78947baab0bc70f3cca7c0d51c3e3
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-10-26 19:05:16 +0100

    Update

--- src/components/MediaListCard.jsx
+++ src/components/MediaListCard.jsx
@@ -1,12 +1,9 @@
 import { useState, useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
 import "./MediaListCard.css";
 import DataJson from "../data/data.json";
 
 function MediaListCard() {
   const [jsonData, setJsonData] = useState(DataJson[0].profileDetails);
-
-  //   const navigate = useNavigate();
 
   useEffect(() => {
     setJsonData(jsonData);
