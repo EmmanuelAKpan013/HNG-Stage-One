3ec83e88ccb33fc35adc078e3ac39c448537c072
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-11-04 13:33:21 +0100

    Feature: Added First and Last Name Field

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,7 +1,37 @@
 import "./ContactPage.css";
 
 function ContactPage() {
-  return <div>ContactPage</div>;
+  return (
+    <div className="contact-me_container">
+      <div className="contact-me_section">
+        <h2>Contact Me</h2>
+        <p>Hi there, contact me to ask me about anything you have in mind.</p>
+        <form action="">
+          <div className="first-last-name">
+            <div className="name-container">
+              <label htmlFor="firstname">First name</label>
+              <input
+                type="text"
+                name="firstname"
+                id="firstname"
+                placeholder="Enter your first name"
+                className="name-field"
+              />
+            </div>
+            <div className="name-container">
+              <label htmlFor="lastname">Last name</label>
+              <input
+                type="text"
+                name="lastname"
+                id="lastname"
+                className="name-field"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default ContactPage;
