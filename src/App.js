4202a154116873d5a51f4989d812eeed26ee7d51
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-11-04 06:57:16 +0100

    Feature: Creating a Contact Page and Routing it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProfilePage from "./Pages/ProfilePage.jsx";
+import ContactPage from "./Pages/ContactPage.jsx";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<ProfilePage />}></Route>
-          <Route path="/profile" element={<ProfilePage />}></Route>
+          <Route path="/" element={<ProfilePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/contact" element={<ContactPage />} />
         </Routes>
       </Router>
     </>
---
+++ src/Pages/ContactPage.jsx
@@ -0,0 +1,7 @@
+import "./ContactPage.css";
+
+function ContactPage() {
+  return <div>ContactPage</div>;
+}
+
+export default ContactPage;
